a6642807bf4039ebc7bb87fe7e32d547cbcadfc3
Ryan Appel <120493240@fvtc.edu>
2025-05-08 15:20:39 -0500

    :bug: fixed scorm routes

--- src/api/v1/api-routes.js
+++ src/api/v1/api-routes.js
@@ -7,5 +7,6 @@ router.use(express.json())
 router.use('/auth', require('./routes/auth-routes'))
 router.use('/courses', require('./routes/course-routes'))
 router.use('/terms', require('./routes/term-routes'))
+router.use('/scorm', require('./routes/scorm-routes'))
 
 module.exports = router
